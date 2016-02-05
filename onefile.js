203943514b9016e561894be06c0f6750da4ebfc0
Phil Rajchgot <tophil@outlook.com>
2016-02-05 18:55:12 +0000

    Removing legacy modules, added search(), cleaned up code, made email questions builder

--- onefile.js
+++ onefile.js
@@ -26,7 +26,7 @@ function start () {
                 create();
                 break;
             case 'Search':
-                //search();
+                search();
                 break;
             case 'Quit':
                 console.log("Goodbye!");
@@ -38,7 +38,7 @@ function start () {
 //start the program
 start();
 
-/******************************create.js**************************************/
+/******************************create**************************************/
 
 //Object.assign() polyfill
 if (typeof Object.assign != 'function') {
@@ -65,39 +65,9 @@ if (typeof Object.assign != 'function') {
   })();
 }
 
-var emailQuestions = [
-    {
-        name: "emailTypes",
-        message: "Types of email...",
-        type: "checkbox",
-        choices: ["home", "work", "other"]
-    },
-    {
-        name: "homeEmail",
-        message: "Home email address...",
-        when: function (answers) {
-            return answers.emailTypes.indexOf("home") !== -1;
-        }
-    },
-    {
-        name: "workEmail",
-        message: "Work email address...",
-        when: function (answers) {
-            return answers.emailTypes.indexOf("work") !== -1;
-        }
-    },
-    {
-        name: "otherEmail",
-        message: "Other email address...",
-        when: function (answers) {
-            return answers.emailTypes.indexOf("other") !== -1;
-        }
-    }
-];
-
 //run functions in sequence, passing result from previous to next function
-//reference to mainMenu function also passed to last function in the sequence
-function  create (defaults) {
+//can take defaults object (entry to use for specifying default values)
+function create (defaults) {
     async.waterfall( [
         async.apply(makeEntry, defaults),
         makeAddresses,
@@ -130,20 +100,22 @@ function makeAddresses (entry, defaults, callback) {
         name: "addressTypes",
         message: "Types of addresses...",
         type: "checkbox",
-        choices: ["home", "work", "other"]
+        choices: ["home", "work", "other"],
+        default: defaults.addressTypes
     }
     
     //prompt the user about which types of addresses they want to add
     inquirer.prompt(typeQuestion, function (typeAnswers) {
-        var addressTypes = typeAnswers.addressTypes;
-        var addressQuestions = buildAddressQuestions(addressTypes, defaults);
+        entry.addressTypes = typeAnswers.addressTypes;
+        var addressQuestions = buildAddressQuestions(typeAnswers.addressTypes, defaults);
         
         //prompt user for address details using customized address questions
         inquirer.prompt(addressQuestions, function (addressAnswers) {
             //for each selected address type, copy details to entry
-            addressTypes.forEach( function (type) {
-                entry = Object.assign(entry, addressAnswers);
-            });
+            // addressTypes.forEach( function (type) {
+            //     entry = Object.assign(entry, addressAnswers);
+            // });
+            entry = Object.assign(entry, addressAnswers);
             
             callback(null, entry, defaults);
         });         
@@ -155,20 +127,22 @@ function makePhones (entry, defaults, callback) {
         name: "phoneTypes",
         message: "Types of phone numbers...",
         type: "checkbox",
-        choices: ["home", "work", "other"]
+        choices: ["home", "work", "other"],
+        default: defaults.phoneTypes
     } 
     
     //prompt the user about which types of phone numbers they want to add
     inquirer.prompt(typeQuestion, function (typeAnswers) {
-        var phoneTypes = typeAnswers.phoneTypes;
-        var phoneQuestions = buildPhoneQuestions(phoneTypes, defaults);
+        entry.phoneTypes = typeAnswers.phoneTypes;
+        var phoneQuestions = buildPhoneQuestions(typeAnswers.phoneTypes, defaults);
         
         //prompt user for phone numbers using customized questions
         inquirer.prompt(phoneQuestions, function (phoneAnswers) {
             //for each selected type of phone number, copy details into entry
-            phoneTypes.forEach( function (type) {
-                entry = Object.assign(entry, phoneAnswers);
-            });
+            // phoneTypes.forEach( function (type) {
+            //     entry = Object.assign(entry, phoneAnswers);
+            // });
+            entry = Object.assign(entry, phoneAnswers);
             
             callback(null, entry, defaults);
         });              
@@ -176,19 +150,42 @@ function makePhones (entry, defaults, callback) {
 }
 
 function makeEmails (entry, defaults, callback) {
-    inquirer.prompt(emailQuestions, function (emailAnswers) {
-        delete emailAnswers.emailTypes;
+    var typeQuestion = {
+        name: "emailTypes",
+        message: "Types of email...",
+        type: "checkbox",
+        choices: ["home", "work", "other"],
+        default: defaults.emailTypes
+    }
+    
+    inquirer.prompt(typeQuestion, function (typeAnswers) {
+        entry.emailTypes = typeAnswers.emailTypes;
+        var emailQuestions = buildEmailQuestions(typeAnswers.emailTypes, defaults);
         
-        entry = Object.assign(entry, emailAnswers);
-        addressBook.push(entry);
-        
-        var entryIndex = addressBook.length - 1;
-        callback(null, entryIndex);
-    });   
+        inquirer.prompt(emailQuestions, function (emailAnswers) {
+            entry = Object.assign(entry, emailAnswers);
+            var entryIndex;
+            
+            //if new entry was based on defaults, 
+            //we have an outdated entry to replace.
+            if (defaults) {
+                var oldIndex = addressBook.indexOf(defaults);
+                addressBook.splice(oldIndex, 1, entry); //replace old with new
+                entryIndex = oldIndex;
+            }
+            else {
+                addressBook.push(entry);
+                entryIndex = addressBook.length - 1;
+            }
+
+            //pass index of entry to the view function
+            callback(null, entryIndex);            
+        });
+    });
 }
 
 ///////////////////// __
-//BUILDER FUNCTIONS//   |
+//BUILDER FUNCTIONS//   \
 /////////////////////   V
 
 function buildInitialQuestions (defaults) {
@@ -220,15 +217,15 @@ function buildAddressQuestions (addressTypes, defaults) {
     var genericAddressQuestions = [
         {
             name: "Line1",
-            message: "address line 1...",
+            message: "address line 1..."
         },
         {
             name: "Line2",
-            message: "address line 2... (optional)",
+            message: "address line 2... (optional)"
         },
         {
             name: "City",
-            message: "city...",
+            message: "city..."
         },
         {
             name: "Province",
@@ -302,9 +299,35 @@ function buildPhoneQuestions (phoneTypes, defaults) {
     return phoneQuestions;    
 }
 
+function buildEmailQuestions (emailTypes, defaults) {
+    defaults = defaults || {};
+    
+    var genericEmailQuestions = {
+        name: "Email",
+        message: "email address...",
+    };
+    
+    var emailQuestions = [];
+    
+    //for each selected type of phone number (home, etc)...
+    emailTypes.forEach( function (type) {
+        //go through the generic questions to build the customized versions
+            var newQuestion = {};
+            
+            newQuestion.name = type + genericQuestion.name;
+            newQuestion.message = type + " " + genericQuestion.message;
+            newQuestion.default = defaults[type + genericQuestion.name];
+            
+            emailQuestions.push(newQuestion);
+        });
+    });    
+    
+    return emailQuestions;
+}
 
 
-/*********************************view.js*************************************/
+
+/*********************************view*************************************/
 
 var viewMenu = [
     {
@@ -318,7 +341,7 @@ var viewMenu = [
         message: "Are you sure you want to delete this entry?",
         type: "confirm",
         when: function (answers) {
-            
+            return answers.viewChoice === "Delete";
         }
     }
 ];
@@ -326,12 +349,11 @@ var viewMenu = [
 function view (index) {
     var table = new Table();
     
-    // console.log(addressBook[index]);
     var entry = addressBook[index];
     var keys = Object.keys(entry);
     
     //DON'T FORGET FOR LATER...
-    //in the object.assign in create.js, add properties like firstNameLabel
+    //in the object.assign in create(), add properties like firstNameLabel
     //with the value of "First Name" to the entry object, so that later you can
     //quickly pull out readable english keys instead of firstName, lastName...
     //then you would do...
@@ -349,12 +371,82 @@ function view (index) {
     inquirer.prompt(viewMenu, function (answers) {
          switch (answers.viewChoice) {
             case "Edit":
-                 
+                create(entry);
+                break;
             case "Delete":
-                
+                if (answers.deleteConfirmation === true) {
+                    addressBook.splice(index, 1);
+                    start();
+                } else { 
+                    view(index);
+                }
+                break;
             case "Go back to main menu":
                 start();
                 break;
          }
+    });
+}
+
+/******************************search**************************************/
+var searchMenu = {
+        name: "query",
+        message: "Enter the search terms..."
+}
+
+function search() {
+    inquirer.prompt(searchMenu, function (answers) {
+        //filter out matching entries
+        var matches = addressBook.filter( function (person) {
+            var keys = Object.keys(person);
+            
+            //returns true immediately if any property value in this person's
+            //entry matches the search query
+            return keys.some( function (key) {
+                if (person[key].indexOf(answers.query) !== -1) {
+                    return true; 
+                }
+                else {
+                    return false;
+                }
+            });
+        });
+        
+        
+        
+        var matchesMenu = {
+            name: "matchesChoice",
+            message: "Select a match...",
+            type: "list",
+            choices: []
+        };
+        
+        //if there are matches...
+        if (matches.length > 0) {
+            //push their names into the list choices, prepended with the match index
+            matches.forEach( function (match, index) {
+                matchesMenu[0].choices.push(index + ". " + match.lastName + ", " + match.firstName);
+            });
+        }
+        else matchesMenu[0].message = "No matches found.";
+            
+        matchesMenu[0].choices.push("Search again", "Back to main menu");
+        
+        inquirer.prompt(matchesMenu, function (answers) {
+            switch (answers.matchesChoice) {
+                case "Search again":
+                    search();
+                    break;
+                case "Back to main menu":
+                    start();
+                    break;
+                default:
+                    var match = matches[answers.matchesChoice.charAt(0)];
+                    var entryIndex = addressBook.indexOf(match);
+                    
+                    view(entryIndex);
+                    break;
+            }
+        });
     });
 }
